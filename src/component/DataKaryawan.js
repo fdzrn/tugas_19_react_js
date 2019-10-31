58c08f906bc78cf3c2917343564300ca7a5adcc5
fdzrn <fadzriangibran@gmail.com>
2019-10-31 16:40:57 +0700

    Mengedit DataBase

--- src/component/DataKaryawan.js
+++ src/component/DataKaryawan.js
@@ -1,18 +1,25 @@
-import React, {Component,Fragment} from 'react';
+import React, {
+	Component,
+	Fragment
+} from 'react';
 import Karyawan from "../Lib/Karyawan";
 import axios from 'axios';
-import {Form,Divider} from 'semantic-ui-react';
+import {
+	Form,
+	Divider
+} from 'semantic-ui-react';
+
 class DataKaryawan extends Component {
 	state = {
 		worker: [],
 		form: {
-        id:0,
-        nama_karyawan:'',
-        jabatan:'',
-        jenis_kelamin:'',
-        tanggal_lahir:'' 
-    },
-    isUpdate: false
+			id: 0,
+			nama_karyawan: '',
+			jabatan: '',
+			jenis_kelamin: '',
+			tanggal_lahir: ''
+		},
+		isUpdate: false
 	}
 
 	getApi = () => {
@@ -24,34 +31,34 @@ class DataKaryawan extends Component {
 	}
 
 	putDataToApi = () => {
-		axios.put(`http://localhost:3004/karyawan/${this.state.form.id}`,this.state.form).then(result => {
+		axios.put(`http://localhost:3004/karyawan/${this.state.form.id}`, this.state.form).then(result => {
 			console.log(result);
 			this.getApi();
 			this.setState({
 				isUpdate: false,
 				form: {
-	        id:0,
-	        nama_karyawan:'',
-	        jabatan:'',
-	        jenis_kelamin:'',
-	        tanggal_lahir:'' 
-      	}
+					id: 0,
+					nama_karyawan: '',
+					jabatan: '',
+					jenis_kelamin: '',
+					tanggal_lahir: ''
+				}
 			})
 		})
 	}
 
-	postDataToApi(){
+	postDataToApi() {
 		axios.post(`http://localhost:3004/karyawan`, this.state.form).then(result => {
 			console.log(result)
 			this.getApi();
 			this.setState({
 				isUpdate: false,
-				form : {
-					id:0,
-	        nama_karyawan:'',
-	        jabatan:'',
-	        jenis_kelamin:'',
-	        tanggal_lahir:'' 
+				form: {
+					id: 0,
+					nama_karyawan: '',
+					jabatan: '',
+					jenis_kelamin: '',
+					tanggal_lahir: ''
 				}
 			})
 		})
@@ -69,13 +76,15 @@ class DataKaryawan extends Component {
 			form: data,
 			isUpdate: true
 		})
-		
+
 	}
 
 	handleFormChange = (e) => {
-		let formNew = {...this.state.form};
+		let formNew = {
+			...this.state.form
+		};
 		let idRandom = new Date().getTime();
-		if(!this.state.isUpdate){
+		if (!this.state.isUpdate) {
 			formNew['id'] = idRandom;
 		}
 		formNew[e.target.name] = e.target.value;
@@ -85,64 +94,103 @@ class DataKaryawan extends Component {
 	}
 
 	handleSave = () => {
-		if(this.state.isUpdate === true){
+		if (this.state.isUpdate === true) {
 			this.putDataToApi();
-		}else{
+		} else {
 			this.postDataToApi();
 		}
-		
+
 	}
 
-	componentDidMount(){
+	componentDidMount() {
 		this.getApi();
 	}
 
 	render() {
-		return(
-			<Fragment>
-			<Divider horizontal> Masukan Data </Divider>
-				<Form>
-		      <Form.Group>
-		        <Form.Input
-		          placeholder='Nama'
-		          name='nama_karyawan'
-		          onChange ={this.handleFormChange}
-		          width={6}
-		          value={this.state.form.nama_karyawan}
-		        />
-		        <Form.Input
-		          placeholder='Jabatan'
-		          name='jabatan'
-		          onChange ={this.handleFormChange}
-		          width={6}
-		          value={this.state.form.jabatan}
-		        />
-		         <Form.Input
-		          placeholder='Jenis Kelamin'
-		          name='jenis_kelamin'
-		          onChange ={this.handleFormChange}
-		          width={6}
-		          value={this.state.form.jenis_kelamin}
-		        />
-		         <Form.Input
-		          placeholder='Tanggal Lahir'
-		          name='tanggal_lahir'
-		          onChange ={this.handleFormChange}
-		          width={6}
-		          value={this.state.form.tanggal_lahir}
-		        />
-		        <Form.Button content='Save' primary onClick={this.handleSave}/>
-		      </Form.Group>
-		    </Form>
-		    <Divider horizontal>Data yang telah tersimpan</Divider>
-		  {
-				this.state.worker.map(worker => {
-				return <Karyawan key={worker.id} data={worker} delete={this.handleRemove} update={this.handleUpdate} />
-				})
-			}	
-	    </Fragment>
-		)
-	}
+		return ( <
+			Fragment >
+			<
+			Divider horizontal > Masukan Data < /Divider>  <
+			Form >
+			<
+			Form.Group >
+			<
+			Form.Input placeholder = 'Nama'
+			name = 'nama_karyawan'
+			onChange = {
+				this.handleFormChange
+			}
+			width = {
+				6
+			}
+			value = {
+				this.state.form.nama_karyawan
+			}
+			/> <
+			Form.Input placeholder = 'Jabatan'
+			name = 'jabatan'
+			onChange = {
+				this.handleFormChange
+			}
+			width = {
+				6
+			}
+			value = {
+				this.state.form.jabatan
+			}
+			/> <
+			Form.Input placeholder = 'Jenis Kelamin'
+			name = 'jenis_kelamin'
+			onChange = {
+				this.handleFormChange
+			}
+			width = {
+				6
+			}
+			value = {
+				this.state.form.jenis_kelamin
+			}
+			/> <
+			Form.Input placeholder = 'Tanggal Lahir'
+			name = 'tanggal_lahir'
+			onChange = {
+				this.handleFormChange
+			}
+			width = {
+				6
+			}
+			value = {
+				this.state.form.tanggal_lahir
+			}
+			/> <
+			Form.Button content = 'Save'
+			primary onClick = {
+				this.handleSave
+			}
+			/> < /
+			Form.Group > <
+			/Form>  
+
+			<
+			Divider horizontal > Data yang telah tersimpan < /Divider> {
+			this.state.worker.map(worker => {
+				return <Karyawan key = {
+					worker.id
+				}
+				data = {
+					worker
+				}
+				delete = {
+					this.handleRemove
+				}
+				update = {
+					this.handleUpdate
+				}
+				/>
+			})
+		} < /Fragment>
+	)
+}
 }
 
 
